daadb526be5ee5b35aabccbe8b852cea2c917404
BroDude3 <github@brodycrowder.com>
2025-03-23 19:27:22 -0500

    Update firebase-init.js

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -5,13 +5,21 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+
+  apiKey: "AIzaSyDVhIJ_abp8jQJi8QAtsnvg-WKkPbNlPqI",
+
+  authDomain: "project-polaris-statefarm.firebaseapp.com",
+
+  projectId: "project-polaris-statefarm",
+
+  storageBucket: "project-polaris-statefarm.firebasestorage.app",
+
+  messagingSenderId: "918316280295",
+
+  appId: "1:918316280295:web:1d5cd9584ad5f1b6f63542",
+
+  measurementId: "G-NFCNZMLTV1"
+
 };
 
 // Initialize Firebase
