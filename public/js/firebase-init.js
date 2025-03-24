2af9159eb61be859792986ef192cae66ad10a719
giratingame <146029359+giratingame@users.noreply.github.com>
2025-03-23 19:28:22 -0500

    Update firebase-init.js

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -27,7 +26,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-// Initialize Firebase Authentication
-const auth = getAuth(app);
-
-export { app, db, analytics, auth, GoogleAuthProvider };
+export { app, db, analytics };
